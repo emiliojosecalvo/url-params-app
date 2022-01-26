54a68640c54b9cedd1b0ac55128b4419758ffe76
Emilio Calvo <emiliojosecalvo@gmail.com>
2022-01-26 12:15:50 +0000

    fix url syntax

--- src/Food.js
+++ src/Food.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react';
 import { useParams } from 'react-router-dom';
 
-export default class Food extends Component {
-    render() {
-        const { name } = useParams();
-        console.log(name);
-        const url = `https://source.unsplash.com/1600x900/?${this.props.name}`
-        return (
-            <div>
-                <h1>This is : {this.props.name}</h1>
-                <img style={{ width: '50%' }} src={url} />
-            </div>
-        )
-    }
+
+function Food() {
+    let name = useParams().name;
+    const url = `https://source.unsplash.com/1600x900/?${name}`;
+    return (
+        <div>
+            <h1>This is : {name}</h1>
+            <img style={{ width: '50%' }} src={url} alt={name} />
+        </div>
+    )
 }
+
+export default Food;
